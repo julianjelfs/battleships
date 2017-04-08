3f8ba22f346d14ecab21a1e078cd87342c9d3fb5
jjelfs <jjelfs@travelrepublic.co.uk>
2017-04-08 14:10:46 +0100

    no idea how to use this reinforcement lib

--- app/js/main.js
+++ app/js/main.js
@@ -15,17 +15,61 @@ function randomMove() {
     return [x,y];
 }
 
+function calculateReward(action) {
+    return 0;
+}
+
+function contains (coordList, coord) {
+    let contains = false;
+    for(let i=0; i<coordList.length; i++) {
+        if(coordList[i][0] === coord[0] && coordList[i][1] === coord[1]){
+            contains = true;
+            break;
+        }
+    }
+    return contains;
+}
+
+function mapState(state) {
+    let coords = [];
+    for(let x=1; x<=10; x++) {
+        for (let y=1; y<=10; y++) {
+            const c = [x,y];
+            const hit = contains(state.hits, c);
+            const miss = contains(state.misses, c);
+            coords.push(hit ? 1 : miss ? -1 : 0);
+        }
+    }
+    return coords;
+}
+
+function trainedMove(state) {
+    currentState = mapState(state);
+    const action = agent.act(currentState);
+    const reward = calculateReward(action);
+    agent.learn(reward);
+    return randomMove();
+}
+
+let currentState = mapState({hits:[], misses:[]});
+
 app.ports.requestMove.subscribe(state => {
     console.log(state);
-    app.ports.receiveMove.send(randomMove());
+    app.ports.receiveMove.send(trainedMove(state));
 });
+
+// This is our reward function
+//  r(a;t0) = ∑t≥t0(h(t)–h(t))(0.5)t−t
+// looks at action a taken at time t0 and returns a
+// weighted sum of hit values h(t) for this and all
+// future steps of the game
 
 const env = {
     getNumStates: function() {
         return 100;
     },
     getMaxNumActions: function() {
-        return 100;
+        return 1;
     },
 };
 
